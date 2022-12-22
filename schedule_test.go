96b623ae757257872d05d47f0cf0d997d004f189
Jonas Tranberg <jonastranberg93@gmail.com>
2022-12-22 12:06:32 +0100

    simplify tests

--- schedule_test.go
+++ schedule_test.go
@@ -9,33 +9,39 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var testTask1 = NewTask(
+	"test",
+	"id",
+	1*time.Second,
+	map[string]any{
+		"foo": "bar",
+	},
+)
+
+func newSchedule(t *testing.T, ctx context.Context, db int) Schedule {
+	t.Helper()
+
+	redisCli := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   db,
+	})
+
+	redisCli.FlushDB(ctx)
+
+	return NewSchedule(redisCli)
+}
+
 func TestScheduleImpl_Add(t *testing.T) {
 	t.Parallel()
 
 	ctx := context.Background()
 
-	redisCli := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-		DB:   1,
-	})
+	schedule := newSchedule(t, ctx, 1)
 
-	redisCli.FlushDB(ctx)
-
-	schedule := NewSchedule(redisCli)
-
-	task := NewTask(
-		"test",
-		"id",
-		1*time.Second,
-		map[string]any{
-			"foo": "bar",
-		},
-	)
-
-	err := schedule.Add(ctx, task)
+	err := schedule.Add(ctx, testTask1)
 	assert.NoError(t, err)
 
-	err = schedule.Add(ctx, task)
+	err = schedule.Add(ctx, testTask1)
 	assert.ErrorIs(t, err, ErrTaskAlreadyExists)
 }
 
@@ -44,31 +50,15 @@ func TestScheduleImpl_Update(t *testing.T) {
 
 	ctx := context.Background()
 
-	redisCli := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-		DB:   2,
-	})
+	schedule := newSchedule(t, ctx, 2)
 
-	redisCli.FlushDB(ctx)
-
-	schedule := NewSchedule(redisCli)
-
-	task := NewTask(
-		"test",
-		"id",
-		1*time.Second,
-		map[string]any{
-			"foo": "bar",
-		},
-	)
-
-	err := schedule.Update(ctx, task)
+	err := schedule.Update(ctx, testTask1)
 	assert.ErrorIs(t, err, ErrTaskDoesNotExist)
 
-	err = schedule.Add(ctx, task)
+	err = schedule.Add(ctx, testTask1)
 	assert.NoError(t, err)
 
-	err = schedule.Update(ctx, task)
+	err = schedule.Update(ctx, testTask1)
 	assert.NoError(t, err)
 }
 
@@ -77,30 +67,14 @@ func TestScheduleImpl_Remove(t *testing.T) {
 
 	ctx := context.Background()
 
-	redisCli := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-		DB:   3,
-	})
+	schedule := newSchedule(t, ctx, 3)
 
-	redisCli.FlushDB(ctx)
-
-	schedule := NewSchedule(redisCli)
-
-	task := NewTask(
-		"test",
-		"id",
-		1*time.Second,
-		map[string]any{
-			"foo": "bar",
-		},
-	)
-
-	err := schedule.Remove(ctx, task.Kind, task.ID)
+	err := schedule.Remove(ctx, testTask1.Kind, testTask1.ID)
 	assert.ErrorIs(t, err, ErrTaskDoesNotExist)
 
-	err = schedule.Add(ctx, task)
+	err = schedule.Add(ctx, testTask1)
 	assert.NoError(t, err)
 
-	err = schedule.Remove(ctx, task.Kind, task.ID)
+	err = schedule.Remove(ctx, testTask1.Kind, testTask1.ID)
 	assert.NoError(t, err)
 }
